454beee96859f816fbb7639f06a2c12ce9e204aa
Dima <dimaberry1@gmail.com>
2022-12-22 18:08:26 +0300

    feat: complete task-tracer

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,15 +1,32 @@
 import { ITodo } from "../types/data";
 
-interface ITodoItem extends ITodo {}
+interface ITodoItem extends ITodo {
+  removeTodo: (id: number) => void;
+  toggleTodo: (id: number) => void;
+}
 
 const TodoItem: React.FC<ITodoItem> = (props) => {
-  const { id, title, complete } = props;
+  const { id, title, complete, removeTodo, toggleTodo } = props;
 
   return (
     <div>
-      <input type="checkbox" checked={complete} />
-      {title}
-      <button>x</button>
+      <input
+        type="checkbox"
+        checked={complete}
+        onChange={() => toggleTodo(id)}
+      />
+      <span style={{ display: "inline-block", margin: "0 10px" }}>{title}</span>
+      <button
+        style={{
+          background: "transparent",
+          border: "none",
+          outline: "none",
+          color: "red",
+        }}
+        onClick={() => removeTodo(id)}
+      >
+        x
+      </button>
     </div>
   );
 };
